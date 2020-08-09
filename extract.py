21c1989968d102b113967bd1fbfaa3ebadec758b
Nadro-J <n4dro@protonmail.com>
2020-08-09 21:53:19 +0000

    checkpoint: blockheader, Flask API: {gobject}

--- extract.py
+++ extract.py
@@ -13,6 +13,11 @@
 #   SELECT * FROM blockinfo WHERE flags LIKE '%proof-of-stake%' AND weight <= 300
 
 import json, requests
+import flask
+from flask import request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
 class rpc:
     def __init__(self):
@@ -31,19 +36,82 @@ class rpc:
         body = json.loads(r.text)
         return body['result']
 
-class blockchain:
+    # Set of commands to manage governance objects
+    def gobject(self, *args):
+        build_args = []
+        for value in args:
+            if value != None:
+                build_args.append(value)
+        return self.request('gobject', build_args)
+
+class scrape_chain:
     def __init__(self):
         self.rpc = rpc()
         self.height = self.getblockheight()
         self.blocks_processed = 0
         self.blockinfo_construct = {}
+        self.blockstats_construct = {}
+        self.blockheader_construct = {}
 
     def getblockheight(self):
         return self.rpc.request('getblockcount')
 
-#    def getblockstats(self):
-#        block_stats = self.rpc.request("getblockstats", [self.blocks_processed])
-#        return block_stats
+    def getblockheader(self):
+        """
+        RETURN: json object
+        Perform RPC commands getblockheader and dump the output into json
+
+        JSON Keys:
+            hash                    nTx
+            confirmations           nextblockhash
+            strippedsize            chainlock
+            size                    previousblockhash
+            weight                  versionHex
+            height                  chainwork
+            version                 difficulty
+            bits                    nonce
+            merkleroot              mediantime
+            time
+        """
+        blockheader = self.rpc.request("getblockhash", [self.blocks_processed])
+        blockheader_data = self.rpc.request("getblockheader", [blockheader])
+
+        try:
+            for key in blockheader_data:
+                self.blockheader_construct[key] = blockheader_data[key]
+            return json.loads(json.dumps(self.blockheader_construct))
+        except Exception as error:
+            return f"Exception raised on getblockheader():\n {error}"
+
+    def getblockstats(self):
+        """
+        RETURN: json object
+        Perform RPC commands getblockstats and dump the output into json
+
+        JSON Keys:
+            avgfee                  avgfeerate
+            avgtxsize               blockhash
+            feerate_percentiles     height
+            ins                     maxfee
+            maxfeerate              maxtxsize
+            medianfee               mediantime
+            mediantxsize            minfee
+            minfeerate              mintxsize
+            outs                    subsidy
+            swtotal_size            swtotal_weight
+            swtxs                   time
+            total_out               total_size
+            total_weight            totalfee
+            txs                     utxo_increase
+            utxo_size_inc
+        """
+        block_stats = self.rpc.request("getblockstats", [self.blocks_processed])
+        try:
+            for key in block_stats:
+                self.blockstats_construct[key] = block_stats[key]
+            return json.loads(json.dumps(self.blockstats_construct))
+        except Exception as error:
+            return f"Exception raised on getblockstats():\n {error}"
 
     def blockchaininfo(self):
         """
@@ -92,15 +160,31 @@ class blockchain:
                     self.blockinfo_construct[key] = str(block[key]).translate(str.maketrans('', '', '[]')).translate(str.maketrans({"'": None}))
         except Exception as error:
             return f"Exception raised on getblockinfo():\n {error}"
-
         return json.loads(json.dumps(self.blockinfo_construct))
 
     def process_blocks(self):
         while self.blocks_processed < self.height:
-            print(self.blockchaininfo())  # printing to debug
+
+            # visual debugging/capturing of block data
+            print(f"▓▓ HEIGHT: {self.blocks_processed} ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+            print(self.blockchaininfo())
+            print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+            print(self.getblockstats())
+            print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+            print(self.getblockheader())
+            print("\n")
 
             self.blocks_processed = self.blocks_processed + 1
 
+@app.route('/api/v1/gobject', methods=['GET'])
+def gobject_API():
+    cmd = request.args.get('cmd')
+    arg = request.args.get('arg')
+    return rpc().gobject(cmd, arg)
+
 if __name__ == '__main__':
-    chain = blockchain()
-    chain.process_blocks()
+    app.run(host='0.0.0.0', port=5000)
+
+    # commented out whilst messing around with Flask API.
+    #chain = scrape_chain()
+    #chain.process_blocks()
