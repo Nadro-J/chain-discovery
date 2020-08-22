95b395564b2107b5d39991ed9c57affb0c1d6aaf
Nadro-J <n4dro@protonmail.com>
2020-08-22 02:14:38 +0000

    Flask API -> Wallet RPC calls: governanceinfo, masternodelist, masternode, blockchaininfo, blockhash, blockheader, block, blockstats, blockcount and spork

--- main.py
+++ main.py
@@ -15,14 +15,60 @@ def gobject_API():
     arg = request.args.get('arg')
     return rpc.gobject(cmd, arg)
 
+@app.route('/api/v1/getgovernanceinfo', methods=['GET'])
+def getgovernanceinfo_API():
+    return rpc.getgovernanceinfo()
+
 @app.route('/api/v1/masternodelist', methods=['GET'])
 def masternodelist_API():
     mode = request.args.get('mode')
     filter = request.args.get('filter')
     return rpc.masternodelist(mode, filter)
 
-if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000)
+@app.route('/api/v1/masternode', methods=['GET'])
+def masternode_API():
+    cmd = request.args.get('cmd')
+    arg = request.args.get('arg')
+    return rpc.masternode(cmd, arg)
 
-    chain = block_extract.scrape_chain()
-    chain.process_blocks()
+@app.route('/api/v1/getblockchaininfo', methods=['GET'])
+def getblockchaininfo_API():
+    return rpc.getblockchaininfo()
+
+@app.route('/api/v1/getblockhash', methods=['GET'])
+def getblockhash_API():
+    block_height = request.args.get('height')
+    return rpc.getblockhash(block_height)
+
+@app.route('/api/v1/getblockheader', methods=['GET'])
+def getblockheader_API():
+    block_height = request.args.get('hash')
+    verbose = request.args.get('verbose')
+    return rpc.getblockheader(block_height, verbose)
+
+@app.route('/api/v1/getblock', methods=['GET'])
+def getblock_API():
+    hash = request.args.get('hash')
+    verbose = request.args.get('verbose')
+    return rpc.getblock(hash, verbose)
+
+@app.route('/api/v1/getblockstats', methods=['GET'])
+def getblockstats_API():
+    hash = request.args.get('hash')
+    verbose = request.args.get('verbose')
+    return rpc.getblockstats(hash, verbose)
+
+@app.route('/api/v1/getblockcount', methods=['GET'])
+def getblockcount_API():
+    return rpc.getblockcount()
+
+@app.route('/api/v1/spork', methods=['GET'])
+def spork_API():
+    cmd = request.args.get('cmd')
+    return rpc.spork(cmd)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+    #chain = block_extract.scrape_chain()
+    #chain.process_blocks()
--- src/rpc_request.py
+++ src/rpc_request.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
+import re
 import json
 import requests
 
 class rpc:
     def __init__(self):
         self.url = 'http://rpcuser:rpcpwd@localhost:8331'
+        self.rpc_arguments = []  # Always clear rpc_arguments during every call to the API
 
     def request(self, method, params=None):
         payload = {'id': 1, 'method': method, 'rpc': '1.0'}
@@ -20,17 +21,46 @@ class rpc:
         body = json.loads(r.text)
         return body['result']
 
+    def arg_handler(self, args):
+        self.rpc_arguments.clear()
+        for value in args:
+            if value != None:
+                if re.search('true', value):
+                    self.rpc_arguments.append(bool(value.title()))
+                else:
+                    self.rpc_arguments.append(value)
+        return self.rpc_arguments
+
     # Set of commands to manage governance objects
     def gobject(self, *args):
-        gobj_args = []
-        for value in args:
-            if value != None:
-                gobj_args.append(value)
-        return self.request('gobject', gobj_args)
+        return self.request('gobject', self.arg_handler(args))
+
+    def getgovernanceinfo(self):
+        return self.request('getgovernanceinfo')
 
     def masternodelist(self, *args):
-        mnlist_args = []
-        for value in args:
-            if value != None:
-                mnlist_args.append(value)
-        return self.request('masternodelist', mnlist_args)
+        return self.request('masternodelist', self.arg_handler(args))
+
+    def masternode(self, *args):
+        return self.request('masternode', self.arg_handler(args))
+
+    def getblockchaininfo(self):
+        return self.request('getblockchaininfo')
+
+    def getblockhash(self, block_height):
+        return self.request('getblockhash', [int(block_height)])
+
+    def getblockheader(self, *args):
+        return self.request('getblockheader', self.arg_handler(args))
+
+    def getblock(self, *args):
+        return self.request('getblock', self.arg_handler(args))
+
+    def getblockstats(self, *args):
+        return self.request('getblockstats', self.arg_handler(args))
+
+    def getblockcount(self):
+        return str(self.request('getblockcount'))
+
+    def spork(self, *args):
+        return self.request('spork', self.arg_handler(args))
